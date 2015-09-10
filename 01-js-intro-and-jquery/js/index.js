de67b5403679c1efaa6fe0bff100d6a983229804
agustinjm <juan.agustinmoreno@gmail.com>
2015-09-10 17:33:25 -0300

    Files relacionados al topic-1

---
+++ 01-js-intro-and-jquery/js/index.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+			// Practice - Point 5.
+			$("#mainSection").hide().fadeIn(2000, function(){
+
+				// Practice - Point 6.
+				$(".alias").focus();
+			});
+
+		// Manejo del evento click del boton
+		$("#btnWelcome").click(function(){
+
+			// Practice - Point 7.
+			var nombreUsuario = $(".alias").val();
+			$.get("http://bootcamp.aws.af.cm/welcome/" + nombreUsuario, manejarRespuesta).error(manejarErrores);
+
+			$("#artist").focus();
+
+		});
+
+		$("#btnSearchAlbums").click(function(){
+			// Recuperacion del nombre del artista ingresado por el usuario
+			if ($("#artist").val() === "")
+			{
+				alert("Please enter an artist name");
+			}
+
+			else
+			{
+				var nombre = $("#artist").val();
+
+				$.getJSON('https://api.spotify.com/v1/search', 
+					{						
+						// Practice - Point 11
+						// q: 'Rolling Stones' 
+						
+						// Practice - Point 13
+						q: nombre,
+						type: 'Album'
+					}, 
+					function(json, textStatus) {
+						
+						// Practice - Point 11
+						for (var i = 0 ; i < json.albums.items.length; i++) 
+						{
+							var article = "<article>";
+					        var title ="<div class=\"column full\"><header><h3>"+ json.albums.items[i].name +"</h3></header></div>";
+					        var type="<div class=\"column full\">"+ json.albums.items[i].type +"</div>";
+					        var image="<div class=\"column full\"><img src=\""+ json.albums.items[i].images[0].url +"\" alt= \"Imagen Disco\"></div>";
+					        var link="<div class=\"column full\"><a href=\""+ json.albums.items[i].external_urls.spotify +"\" alt= \"Link a spotify\"> Watch it on Spotify </a></div></article>";			        
+
+							$("#albums").append(article+title+type+image+link); 
+						} // end for
+					}// end function success json				
+				);// end getJSON
+			}			
+		}); // end click function
+
+}); // end ready.
+
+	
+// Practice - Point 7, 8 and 10
+function manejarRespuesta(data) {
+	var nombre = data.response.slice(8);
+	var mensaje = data.response.substr(0, 8);
+	$("#mensaje").html("<p>" + mensaje + "<strong>" + nombre + "<strong></p>");
+} // end manejarRespuesta
+
+	
+// Practice - Point 9
+function manejarErrores() {
+	$("#mainSection").css("color","#F00");
+} // end manejarErrores
